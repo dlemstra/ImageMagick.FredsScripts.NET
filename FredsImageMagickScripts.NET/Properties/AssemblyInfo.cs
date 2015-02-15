d7d8f22de75d8c3b3471f9d2e1b125e893320492
dlemstra <dlemstra@users.noreply.github.com>
2015-02-15 13:03:01 +0100

    Created projects and solution.

---
+++ FredsImageMagickScripts.NET/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿//=================================================================================================
+// Copyright 2015 Dirk Lemstra, Fred Weinhaus <https://github.com/dlemstra/FredsImageMagickScripts.NET>
+//
+// These scripts are available free of charge for non-commercial use, ONLY.
+//
+// For use of scripts in commercial (for-profit) environments or non-free applications, please
+// contact Fred Weinhaus for licensing arrangements. His email address is fmw at alink dot net.
+//
+// If you:
+// 1. redistribute
+// 2. incorporate any of these scripts into other free applications or
+// 3. reprogram them in another scripting language, then you must contact Fred Weinhaus for permission,
+// especially if the result might be used in a commercial or for-profit environment.
+//
+// Usage, whether stated or not in the script, is restricted to the above licensing arrangements.
+// It is also subject, in a subordinate manner, to the ImageMagick license, which can be found at:
+// http://www.imagemagick.org/script/license.php
+//=================================================================================================
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+//=================================================================================================
+[assembly: AssemblyTitle("FredsImageMagickScripts.NET")]
+[assembly: AssemblyProduct("FredsImageMagickScripts.NET")]
+[assembly: AssemblyDescription("FredsImageMagickScripts.NET")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © 2015 Dirk Lemstra, Fred Weinhaus")]
+[assembly: AssemblyTrademark("")]
+//=================================================================================================
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+//=================================================================================================
