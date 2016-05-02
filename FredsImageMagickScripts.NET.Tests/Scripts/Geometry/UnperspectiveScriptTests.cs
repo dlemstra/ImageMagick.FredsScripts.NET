e180b76c947274e1d5ef153d943537f3d369a458
dlemstra <dlemstra@users.noreply.github.com>
2016-05-02 23:28:07 +0200

    Forgot to add unit tests to the repository.

---
+++ FredsImageMagickScripts.NET.Tests/Scripts/Geometry/UnperspectiveScriptTests.cs
@@ -0,0 +1,285 @@
+﻿//=================================================================================================
+// Copyright 2015 Dirk Lemstra, Fred Weinhaus
+// <https://github.com/dlemstra/FredsImageMagickScripts.NET>
+//
+// These scripts are available free of charge for non-commercial use, ONLY.
+//
+// For use of these scripts in commercial (for-profit) environments or non-free applications,
+// please contact Fred Weinhaus (fmw at alink dot net) for licensing arrangements.
+//
+// If you: 1) redistribute, 2) incorporate any of these scripts into other free applications or
+// 3) reprogram them in another scripting language, then you must contact Fred Weinhaus for
+// permission, especially if the result might be used in a commercial or for-profit environment.
+//
+// Usage, whether stated or not in the script, is restricted to the above licensing arrangements.
+// It is also subject, in a subordinate manner, to the ImageMagick license, which can be found at:
+// http://www.imagemagick.org/script/license.php
+//=================================================================================================
+
+using System;
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FredsImageMagickScripts.NET.Tests.Scripts.Effect
+{
+  [TestClass]
+  public class UnperspectiveScriptTests : ScriptTester
+  {
+    private const string _Category = "UnperspectiveScriptTests";
+
+    private static void Test_Defaults(UnperspectiveScript script, UnperspectiveMethod method)
+    {
+      Assert.AreEqual(null, script.AspectRatio);
+      Assert.AreEqual(0, script.BorderColorLocation.X);
+      Assert.AreEqual(0, script.BorderColorLocation.Y);
+      Assert.AreEqual(0, script.Blur);
+      Assert.AreEqual(UnperspectiveDefault.EdgeLength, script.Default);
+      Assert.AreEqual(false, script.DisableViewportCrop);
+      Assert.AreEqual(null, script.Height);
+      Assert.AreEqual(10, script.MinLength);
+      Assert.AreEqual(40, script.MaxPeaks);
+      Assert.AreEqual(null, script.Rotation);
+      Assert.AreEqual(null, script.Width);
+
+      if (method == UnperspectiveMethod.Peak)
+      {
+        Assert.AreEqual(5.0, script.Sharpen);
+        Assert.AreEqual(1.0, script.Smooth);
+        Assert.AreEqual(4, script.Threshold);
+      }
+      else
+      {
+        Assert.AreEqual(0.0, script.Sharpen);
+        Assert.AreEqual(5.0, script.Smooth);
+        Assert.AreEqual(10, script.Threshold);
+      }
+    }
+
+    private void Test_Defaults(UnperspectiveMethod method)
+    {
+      var script = new UnperspectiveScript(method);
+      Test_Defaults(script, method);
+
+      script.AspectRatio = 1.5;
+      script.BorderColorLocation = new PointD(10, 10);
+      script.Blur = 15.0;
+      script.ColorFuzz = (Percentage)5.0;
+      script.Default = UnperspectiveDefault.BoundingBoxWidth;
+      script.DisableViewportCrop = true;
+      script.Height = 140;
+      script.MaxPeaks = 25;
+      script.MinLength = 5;
+      script.Rotation = UnperspectiveRotation.Rotate180;
+      script.Sharpen = 8.5;
+      script.Smooth = 2.0;
+      script.Threshold = 5;
+      script.Width = 50;
+
+      script.Reset();
+      Test_Defaults(script, method);
+    }
+
+    private void Test_Execute(string fileName, Action<UnperspectiveScript> action, string output)
+    {
+      var input = GetInputFile(fileName);
+      //LosslessCompress(input);
+
+      using (var inputImage = new MagickImage(input))
+      {
+        var script = new UnperspectiveScript();
+        action(script);
+
+        var scriptOutput = script.Execute(inputImage);
+        TestOutput(scriptOutput, output);
+      }
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Defaults()
+    {
+      Test_Defaults(UnperspectiveMethod.Peak);
+      Test_Defaults(UnperspectiveMethod.Derivative);
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Settings()
+    {
+      var script = new AutotrimScript();
+
+      using (var logo = new MagickImage(Images.Logo))
+      {
+        ExceptionAssert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+          script.BorderColorLocation = new PointD(-1, -1);
+          script.Execute(logo);
+        });
+
+        ExceptionAssert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+          script.BorderColorLocation = new PointD(logo.Width, logo.Height);
+          script.Execute(logo);
+        });
+      }
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_mandril2_p30_t30_out()
+    {
+      Test_Execute("mandril2_p30_t30_out.jpg", delegate (UnperspectiveScript script)
+       {
+         script.ColorFuzz = (Percentage)20;
+       }, "mandril2_p30_t30_out_unperspect_f20_a_t4_s1_S5_B0_r0_el_peak.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_mandril2_p30_t30_r60_zc()
+    {
+      Test_Execute("mandril2_p30_t30_r60_zc.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)20;
+      }, "mandril2_p30_t30_r60_zc_unperspect_f20_a_t4_s1_S5_B0_r0_el_peak.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_mandril2_p30_t30_r60_zc_r270()
+    {
+      Test_Execute("mandril2_p30_t30_r60_zc.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)20;
+        script.Rotation = UnperspectiveRotation.Rotate270;
+      }, "mandril2_p30_t30_r60_zc_unperspect_f20_a_t4_s1_S5_B0_r270_el_peak.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_mandril2_pm30_t30_r30_zc()
+    {
+      Test_Execute("mandril2_pm30_t30_r30_zc.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)20;
+      }, "mandril2_pm30_t30_r30_zc_unperspect_f20_a_t4_s1_S5_B0_r0_el_peak.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_mandril2_pm30_t30_r30_zc_f52_t8_s2()
+    {
+      Test_Execute("mandril2_pm30_t30_r30_zc.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)52;
+        script.Threshold = 8;
+        script.Smooth = 2;
+      }, "mandril2_pm30_t30_r30_zc_unperspect_f52_a_t8_s2_S5_B0_r0_el_peak.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_mandril2_pm30_t30_r30_zc_bh()
+    {
+      Test_Execute("mandril2_pm30_t30_r30_zc.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)15;
+        script.Default = UnperspectiveDefault.BoundingBoxHeight;
+      }, "mandril2_pm30_t30_r30_zc_unperspect_f20_a_t4_s1_S5_B0_r0_bh_peak.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_mandril2_pm30_t30_r30_zc_h()
+    {
+      Test_Execute("mandril2_pm30_t30_r30_zc.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)20;
+        script.Default = UnperspectiveDefault.Height;
+      }, "mandril2_pm30_t30_r30_zc_unperspect_f20_a_t4_s1_S5_B0_r0_h_peak.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_mandril2_round30_p30_t30_out()
+    {
+      Test_Execute("mandril2_round30_p30_t30_out.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)20;
+        script.Smooth = 2;
+        script.DisableViewportCrop = true;
+      }, "mandril2_round30_p30_t30_out_unperspect_f20_a_t4_s2_S5_B0_r0_el_peak.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_monet2_p30_t30_r30_out()
+    {
+      Test_Execute("monet2_p30_t30_r30_out.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)10;
+      }, "monet2_p30_t30_r30_out_unperspect_f10_a_t4_s1_S5_B0_r0_el_peak.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_redcanoe_p30_t30_out()
+    {
+      Test_Execute("redcanoe_p30_t30_out.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)10;
+      }, "redcanoe_p30_t30_out_unperspect_f10_a_t4_s1_S5_B0_r0_el_peak.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_receipt1_f7()
+    {
+      Test_Execute("receipt1.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)7;
+        script.DisableViewportCrop = true;
+      }, "receipt1_unperspect_f7_a_t4_s1_S5_B0_r0_el_peak.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_receipt1_f20()
+    {
+      Test_Execute("receipt1.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)20;
+        script.DisableViewportCrop = true;
+      }, "receipt1_unperspect_f20_a_t4_s1_S5_B0_r0_el_peak.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_receipt1_f20_w500()
+    {
+      Test_Execute("receipt1.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)20;
+        script.Width = 500;
+        script.DisableViewportCrop = true;
+      }, "receipt1_unperspect_f20_a_t4_s1_S5_B0_r0_el_peak_w500.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_receipt2()
+    {
+      Test_Execute("receipt2.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)50;
+      }, "receipt2_unperspect_f50_a_t4_s1_S5_B0_r0_el_peak.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_receipt2_w200()
+    {
+      Test_Execute("receipt2.jpg", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)50;
+        script.Width = 200;
+      }, "receipt2_unperspect_f50_a_t4_s1_S5_B0_r0_el_peak_w200.jpg");
+    }
+
+    [TestMethod, TestCategory(_Category)]
+    public void Test_Execute_textsample_localthresh_m1_r25_b5_white_b20_p30_t30_out()
+    {
+      Test_Execute("textsample_localthresh_m1_r25_b5_white_b20_p30_t30_out.png", delegate (UnperspectiveScript script)
+      {
+        script.ColorFuzz = (Percentage)10;
+        script.Threshold = 10;
+        script.Smooth = 2;
+        script.Sharpen = 0;
+        script.Blur = 3;
+      }, "textsample_localthresh_m1_r25_b5_white_b20_p30_t30_out_unperspect27_f10_a_t10_s2_S0_B3_r0_el_peak.jpg");
+    }
+  }
+}
