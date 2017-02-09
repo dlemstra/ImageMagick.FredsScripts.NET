e770a7e6edd4d5c7451cbe04f30da82b5730651b
dlemstra <dlemstra@users.noreply.github.com>
2017-02-09 20:25:07 +0100

    Exit with a failure when the test fail.

--- Tools/AppVeyor/Test.cmd
+++ Tools/AppVeyor/Test.cmd
@@ -3,6 +3,7 @@ call "%vs140comntools%vsvars32.bat"
 
 cd ..\..\
 packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"%VSINSTALLDIR%Common7\IDE\MSTest.exe" -targetargs:"/noresults /noisolation /testcontainer:""tests\FredsImageMagickScripts.NET.Tests\bin\Release\FredsImageMagickScripts.NET.Tests.dll" -register:user -threshold:10 -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -output:.\FredsImageMagickScripts.NET.Coverage.xml
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%
 pip install codecov
