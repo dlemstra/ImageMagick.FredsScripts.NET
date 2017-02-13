f530a94374e27a1288d04b91ac1f93722029be86
dlemstra <dlemstra@users.noreply.github.com>
2017-02-13 01:03:38 +0100

    Refactored the unit tests.

--- tests/FredsImageMagickScripts.NET.Tests/Scripts/Effect/DraganEffectScriptTests.cs
+++ tests/FredsImageMagickScripts.NET.Tests/Scripts/Effect/DraganEffectScriptTests.cs
@@ -27,125 +27,25 @@ namespace FredsImageMagickScripts.NET.Tests
   public class DraganEffectScriptTests : ScriptTester
   {
     [TestMethod]
-    public void Test_Defaults()
+    public void Constructor_SettingsSetToDefaults()
     {
       var script = new DraganEffectScript();
-      Test_Defaults(script);
 
+      AssertDefaults(script);
+    }
+
+    [TestMethod]
+    public void Reset_AllSettingsChanged_RestoredToDefault()
+    {
+      var script = new DraganEffectScript();
       script.Brightness = 0.5;
       script.Contrast = 4;
       script.Darkness = 2;
       script.Saturation = (Percentage)100;
 
       script.Reset();
-      Test_Defaults(script);
-    }
 
-    [TestMethod]
-    public void Test_Execute_before1_draganeffect_b1_c0_d1_s150_r5()
-    {
-      Test_Execute("before1.gif", "before1_draganeffect_b1_c0_d1_s150_r5.jpg", (DraganEffectScript script) =>
-      {
-        script.Brightness = 1;
-        script.Contrast = 0;
-        script.Darkness = 1;
-        script.Saturation = (Percentage)150;
-      });
-    }
-
-    [TestMethod]
-    public void Test_Execute_before1_draganeffect_b1p5_cm5_d1_s175_r5()
-    {
-      Test_Execute("before1.gif", "before1_draganeffect_b1p5_cm5_d1_s175_r5.jpg", (DraganEffectScript script) =>
-      {
-        script.Brightness = 1.5;
-        script.Contrast = -5;
-        script.Darkness = 1;
-        script.Saturation = (Percentage)175;
-      });
-    }
-
-    [TestMethod]
-    public void Test_Execute_before1_draganeffect_b1p5_cm5_d2_s175_r5()
-    {
-      Test_Execute("before1.gif", "before1_draganeffect_b1p5_cm5_d2_s175_r5.jpg", (DraganEffectScript script) =>
-      {
-        script.Brightness = 1.5;
-        script.Contrast = -5;
-        script.Darkness = 2;
-        script.Saturation = (Percentage)175;
-      });
-    }
-
-    [TestMethod]
-    public void Test_Execute_bluehat_draganeffect_b1_c0_d1_s150_r5()
-    {
-      Test_Execute("bluehat.jpg", "bluehat_draganeffect_b1_c0_d1_s150_r5.jpg", (DraganEffectScript script) =>
-      {
-        script.Brightness = 1;
-        script.Contrast = 0;
-        script.Darkness = 1;
-        script.Saturation = (Percentage)150;
-      });
-    }
-
-    [TestMethod]
-    public void Test_Execute_bluehat_draganeffect_b1_cm7p5_d1_s200_r5()
-    {
-      Test_Execute("bluehat.jpg", "bluehat_draganeffect_b1_cm7p5_d1_s200_r5.jpg", (DraganEffectScript script) =>
-      {
-        script.Brightness = 1;
-        script.Contrast = -7.5;
-        script.Darkness = 1;
-        script.Saturation = (Percentage)200;
-      });
-    }
-
-    [TestMethod]
-    public void Test_Execute_bluehat_draganeffect_b1_cm7p5_d1p25_s200_r5()
-    {
-      Test_Execute("bluehat.jpg", "bluehat_draganeffect_b1_cm7p5_d1p25_s200_r5.jpg", (DraganEffectScript script) =>
-      {
-        script.Brightness = 1;
-        script.Contrast = -7.5;
-        script.Darkness = 1.25;
-        script.Saturation = (Percentage)200;
-      });
-    }
-
-    [TestMethod]
-    public void Test_Execute_CHINA_715_4_small_draganeffect_b1_cm5_d1_s150_r5()
-    {
-      Test_Execute("CHINA-715-4_small.jpg", "CHINA-715-4_small_draganeffect_b1_cm5_d1_s150_r5.jpg", (DraganEffectScript script) =>
-      {
-        script.Brightness = 1;
-        script.Contrast = -5;
-        script.Darkness = 1;
-        script.Saturation = (Percentage)150;
-      });
-    }
-
-    [TestMethod]
-    public void Test_Execute_CHINA_715_4_small_draganeffect_b1_cm10_d1_s200_r5()
-    {
-      Test_Execute("CHINA-715-4_small.jpg", "CHINA-715-4_small_draganeffect_b1_cm10_d1_s200_r5.jpg", (DraganEffectScript script) =>
-      {
-        script.Contrast = -10;
-        script.Darkness = 1;
-        script.Saturation = (Percentage)200;
-      });
-    }
-
-    [TestMethod]
-    public void Test_Execute_mustache_draganeffect_b1_cm5_d1p75_s175_r5()
-    {
-      Test_Execute("mustache.jpg", "mustache_draganeffect_b1_cm5_d1p75_s175_r5.jpg", (DraganEffectScript script) =>
-      {
-        script.Brightness = 1;
-        script.Contrast = -5;
-        script.Darkness = 1.75;
-        script.Saturation = (Percentage)175;
-      });
+      AssertDefaults(script);
     }
 
     [TestMethod]
@@ -159,51 +59,168 @@ namespace FredsImageMagickScripts.NET.Tests
     }
 
     [TestMethod]
-    public void Test_Settings()
+    public void Brightness_BelowZero_ThrowsException()
     {
-      var script = new DraganEffectScript();
-
-      using (var logo = new MagickImage(Images.Logo))
+      AssertInvalidOperation("Invalid brightness specified, value must be zero or higher.", (DraganEffectScript script) =>
       {
-        script.Execute(logo);
-
-        ExceptionAssert.Throws<InvalidOperationException>(() =>
-        {
-          script.Brightness = -1.0;
-          script.Execute(logo);
-        });
-
-        ExceptionAssert.Throws<InvalidOperationException>(() =>
-        {
-          script.Reset();
-          script.Contrast = -11.0;
-          script.Execute(logo);
-        });
-
-        ExceptionAssert.Throws<InvalidOperationException>(() =>
-        {
-          script.Reset();
-          script.Contrast = 11.0;
-          script.Execute(logo);
-        });
-
-        ExceptionAssert.Throws<InvalidOperationException>(() =>
-        {
-          script.Reset();
-          script.Darkness = 0.0;
-          script.Execute(logo);
-        });
-
-        ExceptionAssert.Throws<InvalidOperationException>(() =>
-        {
-          script.Reset();
-          script.Saturation = (Percentage)(-1);
-          script.Execute(logo);
-        });
-      }
+        script.Brightness = -1.0;
+      });
     }
 
-    private static void Test_Defaults(DraganEffectScript script)
+    [TestMethod]
+    public void Contrast_BelowMinusTen_ThrowsException()
+    {
+      AssertInvalidOperation("Invalid contrast specified, the range is -10 to 10.", (DraganEffectScript script) =>
+      {
+        script.Contrast = -11.0;
+      });
+    }
+
+    [TestMethod]
+    public void Contrast_AboveTen_ThrowsException()
+    {
+      AssertInvalidOperation("Invalid contrast specified, the range is -10 to 10.", (DraganEffectScript script) =>
+      {
+        script.Contrast = 11.0;
+      });
+    }
+
+    [TestMethod]
+    public void Darkness_Zero_ThrowsException()
+    {
+      AssertInvalidOperation("Invalid darkness specified, value must be 1 or higher.", (DraganEffectScript script) =>
+      {
+        script.Darkness = 0.0;
+      });
+    }
+
+    [TestMethod]
+    public void Saturation_BelowZero_ThrowsException()
+    {
+      AssertInvalidOperation("Invalid saturation specified, value must be zero or higher.", (DraganEffectScript script) =>
+      {
+        script.Saturation = (Percentage)(-1);
+      });
+    }
+
+    [TestMethod]
+    public void Execute_b1_c0_d1_s150_r5_jpg()
+    {
+      AssertExecute("before1.gif", nameof(Execute_b1_c0_d1_s150_r5_jpg), (DraganEffectScript script) =>
+      {
+        script.Brightness = 1;
+        script.Contrast = 0;
+        script.Darkness = 1;
+        script.Saturation = (Percentage)150;
+      });
+    }
+
+    [TestMethod]
+    public void Execute_b1p5_cm5_d1_s175_r5_jpg()
+    {
+      AssertExecute("before1.gif", nameof(Execute_b1p5_cm5_d1_s175_r5_jpg), (DraganEffectScript script) =>
+      {
+        script.Brightness = 1.5;
+        script.Contrast = -5;
+        script.Darkness = 1;
+        script.Saturation = (Percentage)175;
+      });
+    }
+
+    [TestMethod]
+    public void Execute_b1p5_cm5_d2_s175_r5_jpg()
+    {
+      AssertExecute("before1.gif", nameof(Execute_b1p5_cm5_d2_s175_r5_jpg), (DraganEffectScript script) =>
+      {
+        script.Brightness = 1.5;
+        script.Contrast = -5;
+        script.Darkness = 2;
+        script.Saturation = (Percentage)175;
+      });
+    }
+
+    [TestMethod]
+    public void Execute__b1_c0_d1_s150_r5_jpg()
+    {
+      AssertExecute("bluehat.jpg", nameof(Execute__b1_c0_d1_s150_r5_jpg), (DraganEffectScript script) =>
+      {
+        script.Brightness = 1;
+        script.Contrast = 0;
+        script.Darkness = 1;
+        script.Saturation = (Percentage)150;
+      });
+    }
+
+    [TestMethod]
+    public void Execute_b1_cm7p5_d1_s200_r5_jpg()
+    {
+      AssertExecute("bluehat.jpg", nameof(Execute_b1_cm7p5_d1_s200_r5_jpg), (DraganEffectScript script) =>
+      {
+        script.Brightness = 1;
+        script.Contrast = -7.5;
+        script.Darkness = 1;
+        script.Saturation = (Percentage)200;
+      });
+    }
+
+    [TestMethod]
+    public void Execute_b1_cm7p5_d1p25_s200_r5_jpg()
+    {
+      AssertExecute("bluehat.jpg", nameof(Execute_b1_cm7p5_d1p25_s200_r5_jpg), (DraganEffectScript script) =>
+      {
+        script.Brightness = 1;
+        script.Contrast = -7.5;
+        script.Darkness = 1.25;
+        script.Saturation = (Percentage)200;
+      });
+    }
+
+    [TestMethod]
+    public void Execute_b1_cm5_d1_s150_r5_jpg()
+    {
+      AssertExecute("CHINA-715-4_small.jpg", nameof(Execute_b1_cm5_d1_s150_r5_jpg), (DraganEffectScript script) =>
+      {
+        script.Brightness = 1;
+        script.Contrast = -5;
+        script.Darkness = 1;
+        script.Saturation = (Percentage)150;
+      });
+    }
+
+    [TestMethod]
+    public void Execute_b1_cm10_d1_s200_r5_jpg()
+    {
+      AssertExecute("CHINA-715-4_small.jpg", nameof(Execute_b1_cm10_d1_s200_r5_jpg), (DraganEffectScript script) =>
+      {
+        script.Contrast = -10;
+        script.Darkness = 1;
+        script.Saturation = (Percentage)200;
+      });
+    }
+
+    [TestMethod]
+    public void Execute_b1_cm5_d1p75_s175_r5_jpg()
+    {
+      AssertExecute("mustache.jpg", nameof(Execute_b1_cm5_d1p75_s175_r5_jpg), (DraganEffectScript script) =>
+      {
+        script.Brightness = 1;
+        script.Contrast = -5;
+        script.Darkness = 1.75;
+        script.Saturation = (Percentage)175;
+      });
+    }
+
+    [TestMethod]
+    public void Execute_d3_s100_jpg()
+    {
+      AssertExecute("mustache.jpg", nameof(Execute_d3_s100_jpg), (DraganEffectScript script) =>
+      {
+        script.Darkness = 3;
+        script.Saturation = (Percentage)100;
+      });
+    }
+
+    private static void AssertDefaults(DraganEffectScript script)
     {
       Assert.AreEqual(1.0, script.Brightness);
       Assert.AreEqual(0.0, script.Contrast);
@@ -211,7 +228,22 @@ namespace FredsImageMagickScripts.NET.Tests
       Assert.AreEqual((Percentage)150, script.Saturation);
     }
 
-    private void Test_Execute(string input, string output, Action<DraganEffectScript> action)
+    private static void AssertInvalidOperation(string expectedMessage, Action<DraganEffectScript> initAction)
+    {
+      var script = new DraganEffectScript();
+
+      using (var logo = new MagickImage(Images.Logo))
+      {
+        initAction(script);
+
+        ExceptionAssert.Throws<InvalidOperationException>(expectedMessage, () =>
+        {
+          script.Execute(logo);
+        });
+      }
+    }
+
+    private void AssertExecute(string input, string methodName, Action<DraganEffectScript> action)
     {
       string inputFile = GetInputFile(input);
       /* LosslessCompress(inputFile); */
@@ -221,8 +253,11 @@ namespace FredsImageMagickScripts.NET.Tests
         var script = new DraganEffectScript();
         action(script);
 
-        var scriptOutput = script.Execute(image);
-        AssertOutput(scriptOutput, output);
+        using (var scriptOutput = script.Execute(image))
+        {
+          string outputFile = GetInputFile(input, methodName);
+          AssertOutput(scriptOutput, outputFile);
+        }
       }
     }
   }
