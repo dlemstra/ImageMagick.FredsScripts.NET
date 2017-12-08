1d99832c53dc42ccd222c3dc706691a245067702
dlemstra <dlemstra@users.noreply.github.com>
2017-12-08 07:25:40 +0100

    Fixed push script.

--- Publish/Nupkg/Push.cmd
+++ Publish/Nupkg/Push.cmd
@@ -6,7 +6,7 @@ if not %ERRORLEVEL% == 0 goto done
 echo Are you sure?
 pause
 
-for /r %%i in (*.nupkg) do ..\..\Tools\Programs\nuget.exe push %%i %ApiKey%
+for /r %%i in (*.nupkg) do ..\..\Tools\Programs\nuget.exe push %%i %ApiKey% -src nuget.org
 for /r %%i in (*.nupkg) do move %%i ..\..\..\FredsImageMagickScripts.NET.Archive
 
 :done
