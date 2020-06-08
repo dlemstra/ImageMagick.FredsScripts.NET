5a6712179655d1365c91f17d05ee333c1e8e3567
Dirk Lemstra <dirk@lemstra.org>
2020-06-08 07:39:05 +0200

    Whitespace.

--- tools/windows/utils.ps1
+++ tools/windows/utils.ps1
@@ -13,16 +13,16 @@
 
 function checkExitCode($msg)
 {
-  if ($LastExitCode -ne 0)
-  {
-    Write-Error $msg
-    Exit 1
-  }
+    if ($LastExitCode -ne 0)
+    {
+        Write-Error $msg
+        Exit 1
+    }
 }
 
 function fullPath($path)
 {
-  return "$PSScriptRoot\..\..\$path"
+    return "$PSScriptRoot\..\..\$path"
 }
 
 function buildSolution($solution, $properties)
