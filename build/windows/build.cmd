09868641ed7a6912ab562fb05eb885a48d5a7b47
Dirk Lemstra <dirk@lemstra.org>
2020-06-09 08:14:05 +0200

    Renamed build/test files and added publish to the build.

--- build/windows/build.cmd
+++ build/windows/build.cmd
@@ -1,5 +1,5 @@
 @echo off
 call "..\..\tools\windows\init.visualstudio.cmd"
 
-powershell .\test.FredsImageMagickScripts.NET.ps1
+powershell .\build.ps1 -config %1
 if %errorlevel% neq 0 exit /b %errorlevel%
--- build/windows/test.cmd
+++ build/windows/test.cmd
@@ -1,5 +1,5 @@
 @echo off
 call "..\..\tools\windows\init.visualstudio.cmd"
 
-powershell .\build.FredsImageMagickScripts.NET.ps1 -config %1
+powershell .\test.ps1
 if %errorlevel% neq 0 exit /b %errorlevel%
