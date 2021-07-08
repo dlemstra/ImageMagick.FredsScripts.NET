1287c751b4b58f9231400a031b5dee510f9fa133
Dirk Lemstra <dirk@lemstra.org>
2021-07-08 20:46:06 +0200

    Changed header for new repo url.

--- tests/ImageMagick.FredsScripts.NET.Tests/Scripts/Geometry/TshirtScriptTests/TheConstructor.cs
+++ tests/ImageMagick.FredsScripts.NET.Tests/Scripts/Geometry/TshirtScriptTests/TheConstructor.cs
@@ -1,4 +1,4 @@
-﻿// Copyright Dirk Lemstra, Fred Weinhaus (https://github.com/dlemstra/FredsImageMagickScripts.NET)
+﻿// Copyright Dirk Lemstra, Fred Weinhaus (https://github.com/dlemstra/ImageMagick.FredsScripts.NET)
 //
 // These scripts are available free of charge for non-commercial use, ONLY.
 //
